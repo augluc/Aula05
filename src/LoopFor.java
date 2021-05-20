5f682fb8ed8f68145c45d8ff499f7b0486fc45e4
Lucas Assis <lucasagdeassis@gmail.com>
2021-05-19 21:31:57 -0300

    Conceito de loop for

---
+++ src/LoopFor.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LoopFor {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		for(int i=0;i<=100;i=i+2) {
+			System.out.println(i);
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
