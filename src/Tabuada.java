958f3918cf72f1d1a14644c5e5f4e6a5b484f00d
Lucas Assis <lucasagdeassis@gmail.com>
2021-05-19 21:39:47 -0300

    Adição da tabuada a loop for

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class Tabuada {
 
 	public static void main(String[] args) {
-		Scanner leitor = new Scanner(System.in);  
-		
+		Scanner leitor = new Scanner(System.in);
+
 		int numero;
-		//int resultado;
-		
+		// int resultado;
+
 		System.out.println("Por favor, digite o valor do qual você deseja a tabuada:");
-		
+
 		numero = leitor.nextInt();
-		
-		for(int i=1; i<=10; i++) {
-			//resultado = i*numero;
-			System.out.println(numero + " x " + i + " = " + (i*numero));
+
+		for (int i = 1; i <= 10; i++) {
+			// resultado = i*numero;
+			System.out.println(numero + " x " + i + " = " + (i * numero));
 		}
-				
+
 		leitor.close();
 
 	}
